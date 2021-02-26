8b5b2a70c54dbf48705380325e18b1485269498b
Cláudio Sá de Castro <claudio@hiades.com.br>
2021-02-26 16:51:58 -0300

    Fix script used to build RGB SDK library for Linux: new correction.

--- bindings/npm/bin/build_linux_lib.sh
+++ bindings/npm/bin/build_linux_lib.sh
@@ -27,4 +27,4 @@ if [[ ! -d "$RUST_PROJECT_DIR" ]]; then
   exit -1
 fi
 
-cargo build --target x86_64-unknown-linux-gnu --release --manifest-path "$RUST_PROJECT_DIR/Cargo.toml" && rsync -t "$RUST_PROJECT_DIR/target/x86_64-apple-darwin/release"/lib* "$LIB_DIR"/linux/ && rsync -t "$RUST_PROJECT_DIR/librgb.h" "$INCLUDE_DIR"
+cargo build --target x86_64-unknown-linux-gnu --release --manifest-path "$RUST_PROJECT_DIR/Cargo.toml" && rsync -t "$RUST_PROJECT_DIR/target/x86_64-unknown-linux-gnu/release"/lib* "$LIB_DIR"/linux/ && rsync -t "$RUST_PROJECT_DIR/librgb.h" "$INCLUDE_DIR"
